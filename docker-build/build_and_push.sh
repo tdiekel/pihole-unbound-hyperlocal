343785ca9a71ff9fcf899770e2c263e1f1dc102f
Tobias Diekel <tdiekel@mailbox.org>
2021-12-31 13:47:31 +0100

    feat: add jucklul/pihole-updatelists

--- docker-build/build_and_push.sh
+++ docker-build/build_and_push.sh
@@ -1,2 +1,2 @@
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t sujiba/pihole-unbound-hyperlocal:`cat VERSION` --push .
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t sujiba/pihole-unbound-hyperlocal:latest --push .
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tdiekel/pihole-unbound-hyperlocal:`cat VERSION` --push .
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tdiekel/pihole-unbound-hyperlocal:latest --push .
